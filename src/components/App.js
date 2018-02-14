c655396831b8cf5a569575b7816e8d375e6ec5a5
Larry Botha <larry@fixate.it>
2018-02-14 08:42:46 +0200

    add uistate to star wars reducer

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@ import React, {Component} from 'react';
 
 import './App.css';
 
+// wtf... this is magically connected... is this create-react magic?
 class App extends Component {
   render() {
+    const {starWars, fetchStarWarsRequest} = this.props;
+
     return (
       <div>
         <h1>Redux Saga</h1>
         <div>
-          {this.props.starWars.people.map((person, i) => (
-            <h4 key={i}>{person.name}</h4>
-          ))}
+          {starWars.people.map((person, i) => <h4 key={i}>{person.name}</h4>)}
         </div>
-        <button onClick={this.props.fetchStarWarsRequest}>Load More</button>
+        <button onClick={fetchStarWarsRequest}>
+          {starWars.uiState === 'busy' ? 'loading...' : 'Load More'}
+        </button>
       </div>
     );
   }

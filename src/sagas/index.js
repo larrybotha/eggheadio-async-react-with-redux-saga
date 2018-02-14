7e90346765e0ba54eb4dab38900727a0cc993ac5
Larry Botha <larry@fixate.it>
2018-02-14 09:14:21 +0200

    annotate take, race, and all effects

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,7 @@
 // call and put are known as effects in redux-saga
-import {call, put, takeLatest} from 'redux-saga/effects';
+import {call, put, takeLatest, take,
+  // race, all
+} from 'redux-saga/effects';
 
 import api from '../api';
 
@@ -10,16 +12,42 @@ import * as actions from '../actions';
 // when we add a watcher to fetchStarWarsRequest
 function* fetchPerson(action) {
   try {
+    // we can block a request by using take, and only continuing when another
+    // action is dispatched with the correct type
+    console.log('entered fetchPerson')
+    yield take(actions.CONFIRMATION);
+    console.log('after confirm');
+
+    // race is a blocking effect that will return whichever endpoint responds first
+    // It will clean up any effects that are still pending
+    // const {normal, custom} = yield race({
+    //   normal: call(api, '/people'),
+    //   custom: call(api, '/people/justForTyler')
+    // })
+
+    // all is a hybrid effect - it can be blocking or non-blocking. This is
+    // determined by the effects it contains. In this case, because we are using
+    // call inside of all, it is a blocking effect
+    // if all is blocking, it waits for all calls to return a response
+    // const {normal, custom} = yield all({
+    //   normal: call(api, '/people'),
+    //   custom: call(api, '/people/justForTyler')
+    // })
+
     // api is a function which will be called with the remaining
     // parameters spread onto it
-    const person = yield call(api, 'https://swapi.co/api/people');
+    // call is blocking, because we don't want anything else to happend until we
+    // get a result.
+    const person = yield call(api, '/people');
 
     // put takes an action creator
     // put works in the same way as redux's dispatch - it dispatches actions
     // put will only be executed once call returns a result - call is blocking
     // This makes asynchronous requests, like fetching data from an API,
     // synchronous
-    yield put({type: TYPES.FETCH_STAR_WARS_SUCCESS, data: person.results});
+    // put is non-blocking, because at this point don't need to wait for anything
+    // before dispatching an action - we have our data, and we want our store
+    // updated
     yield put({type: actions.FETCH_STAR_WARS_SUCCESS, data: person.results});
   } catch (e) {
     console.log(e);

3f9b318f542701105efb7a4666cf739104f2e3fd
Larry Botha <larry@fixate.it>
2018-02-15 08:52:39 +0200

    add fetch planets actions, reducers, and sagas

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,17 @@ export const CONFIRMATION = 'CONFIRMATION';
 export const confirmFetchStarWars = () => ({
   type: CONFIRMATION,
 });
+
+export const FETCH_STAR_WARS_PLANETS_REQUEST =
+  'FETCH_STAR_WARS_PLANETS_REQUEST';
+export const starWarsPlanetsRequest = () => {
+  return {
+    type: FETCH_STAR_WARS_PLANETS_REQUEST,
+  };
+};
+
+export const FETCH_STAR_WARS_PLANETS_SUCCESS =
+  'FETCH_STAR_WARS_PLANETS_SUCCESS';
+export const starWarsPlanetsSuccess = () => ({
+  type: FETCH_STAR_WARS_PLANETS_SUCCESS,
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,7 @@ import * as actions from '../actions';
 const initialState = {
   uiState: 'idle',
   people: [],
+  planets: [],
 };
 
 const handleStarWarsFetchRequest = (state, action) => {
@@ -21,10 +22,27 @@ const handleStarWarsFetchSuccess = (state, action) => {
   };
 };
 
+const handleStarWarsFetchPlanetsRequest = (state, action) => {
+  return {
+    ...state,
+    uiState: 'busy',
+  };
+};
+
+const handleStarWarsFetchPlanetsSuccess = (state, action) => {
+  return {
+    ...state,
+    planets: action.data,
+    uiState: 'idle',
+  };
+};
+
 const starWars = (state = initialState, action) => {
   const handlers = {
     [actions.FETCH_STAR_WARS_SUCCESS]: handleStarWarsFetchSuccess,
     [actions.FETCH_STAR_WARS_REQUEST]: handleStarWarsFetchRequest,
+    [actions.FETCH_STAR_WARS_PLANETS_REQUEST]: handleStarWarsFetchPlanetsRequest,
+    [actions.FETCH_STAR_WARS_PLANETS_SUCCESS]: handleStarWarsFetchPlanetsSuccess,
   };
   return handlers[action.type] ? handlers[action.type](state, action) : state;
 };

26bc24641cd91c6850a8aeaed32e1f41e8381adb
Larry Botha <larry@fixate.it>
2018-02-16 09:11:37 +0200

    add a forked saga to test non-blocking effects

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,4 +1,4 @@
-import {all, call, fork, put, takeLatest} from 'redux-saga/effects';
+import {all, call, cancel, fork, put, takeLatest} from 'redux-saga/effects';
 
 import api from '../api';
 
@@ -16,6 +16,16 @@ function* fetchPerson(action) {
 
 export function* fetchPersonSaga() {
   yield takeLatest(actions.FETCH_STAR_WARS_REQUEST, fetchPerson);
+}
+
+// this is a non-blocking request on fetchPerson
+export function* forkedFetchPerson() {
+  // we use fork to do a non-blocking call
+  const syncPersons = yield fork(fetchPerson);
+  // and then wait for a STOP_BACKGROUND_FETCH action to be dispatched
+  yield take(actions.STOP_BACKGROUND_FETCH);
+  // at which point we can then cancel the request
+  yield cancel(syncPersons);
 }
 
 export default function* rootSaga() {

4559dabc7d6011f348e103a14ce3ecfe05fdf20c
Larry Botha <larry@fixate.it>
2018-02-16 08:34:23 +0200

    update ui to inspect queued requests

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,29 @@ import './App.css';
 import * as actions from '../actions';
 
 class App extends Component {
+  state = {count: 0};
+
+  handleQueue = () => {
+    this.props.queueChannelRequests();
+    this.setState(({count}) => ({count: count + 1}));
+  };
+
   render() {
+    const {count} = this.state;
+    const {fetchStarWarsRequest, starWars} = this.props;
     const loading = starWars.uiState !== 'idle';
 
     return (
       <div>
         <h1>Redux Saga</h1>
+        <h3># of Button Clicks {count}</h3>
+        <h3># of Saga effects {starWars.people}</h3>
 
         <button onClick={fetchStarWarsRequest}>
           {loading ? 'loading...' : 'people'}
         </button>
 
+        <button onClick={this.handleQueue}>Queue channel</button>
       </div>
     );
   }
